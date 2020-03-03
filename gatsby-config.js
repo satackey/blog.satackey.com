35e86d67a971734b1cc9ccb9f84b577a2a0e9a5d
satackey <21271711+satackey@users.noreply.github.com>
2020-03-03 13:33:52 +0900

    更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,11 @@
 module.exports = {
   siteMetadata: {
-    // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `さなけのなさけ`,
+    author: `さなけ`,
+    description: `技術メモとイベント参加録`,
+    siteUrl: `https://blog.satackey.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `satakenkoki`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -34,11 +34,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong>.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                Twitter
               </a>
             </p>
           </Container>
